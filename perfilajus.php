c4ac65f8b9f95c5ac1caefd0adbaba31843fee65
Sheidel1001 <mari.g.r1001@gmail.com>
2023-10-30 20:32:03 -0500

    prueba

---
+++ perfilajus.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Editar Perfil del Estudiante</title>
+    <link rel="stylesheet" href="./styles/perfil.css">
+    <element class="body">
+</head>
+<body>
+    <h1>Editar Perfil</h1>
+    
+<form action="">
+        <label for="nombre">Nombre de usuario</label>
+        <input type="text" id="name" name="name" required>
+
+        <label for="edad">Edad</label>
+        <input type="number" name="edad" required>
+
+        <label for="info">info</label>
+        <input type="text"  name="info" required>
+
+        <button type="submit">Guardar Cambios</button>
+    </form>
+
+</body>
+</html>
