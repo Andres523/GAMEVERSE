6e4b57ab473500d247e849c90b8bbe603d386350
Andres523 <andresposadat523@gmail.com>
2023-11-22 13:14:57 -0500

    a

--- verificar_correo.php
+++ verificar_correo.php
@@ -43,7 +43,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 ?>
 
 <!DOCTYPE html>
-<html lang="en">WW
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
