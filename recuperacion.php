eb91b703defd57b1e3fa46e9af1a738658f368cc
samux4350 <samug4350@gmail.com>
2023-11-23 19:23:46 -0500

    S

--- recuperacion.php
+++ recuperacion.php
@@ -1,62 +1,13 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="./img/logo.png">
     <link rel="stylesheet" href="./styles/recuper.css">
     <title>Recuperación de Cuenta</title>
 </head>
-<body>
-
-  <!-- Animacion -->
-  <svg version="1.1" xmlns="http://www.w3.org/2000/svg"
-		xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="100%" height="100%" viewBox="0 0 1600 900" preserveAspectRatio="xMidYMax slice">
-		<defs>
-			<linearGradient id="bg">
-                <stop offset="0%" style="stop-color:rgba(204, 102, 0, 0.06)"></stop>
-                <stop offset="50%" style="stop-color:rgba(204, 102, 0, 0.6)"></stop>
-                <stop offset="100%" style="stop-color:rgba(204, 102, 0, 0.2)"></stop>
-			</linearGradient>
-			<path id="wave" fill="url(#bg)" d="M-363.852,502.589c0,0,236.988-41.997,505.475,0
-	s371.981,38.998,575.971,0s293.985-39.278,505.474,5.859s493.475,48.368,716.963-4.995v560.106H-363.852V502.589z" />
-		</defs>
-		<g>
-			<use xlink:href='#wave' opacity=".3">
-				<animateTransform
-          attributeName="transform"
-          attributeType="XML"
-          type="translate"
-          dur="10s"
-          calcMode="spline"
-          values="270 230; -334 180; 270 230"
-          keyTimes="0; .5; 1"
-          keySplines="0.42, 0, 0.58, 1.0;0.42, 0, 0.58, 1.0"
-          repeatCount="indefinite" />
-			</use>
-			<use xlink:href='#wave' opacity=".6">
-				<animateTransform
-          attributeName="transform"
-          attributeType="XML"
-          type="translate"
-          dur="8s"
-          calcMode="spline"
-          values="-270 230;243 220;-270 230"
-          keyTimes="0; .6; 1"
-          keySplines="0.42, 0, 0.58, 1.0;0.42, 0, 0.58, 1.0"
-          repeatCount="indefinite" />
-			</use>
-			<use xlink:href='#wave' opacty=".9">
-				<animateTransform
-          attributeName="transform"
-          attributeType="XML"
-          type="translate"
-          dur="6s"
-          calcMode="spline"
-          values="0 230;-140 200;0 230"
-          keyTimes="0; .4; 1"
-          keySplines="0.42, 0, 0.58, 1.0;0.42, 0, 0.58, 1.0"
-          repeatCount="indefinite" />
-			</use>
-		</g>
-	</svg>
+<body style= "background-color:  #243b55; background-image: linear-gradient(to bottom, rgba(25, 50, 69, 0.86), #000000);">
 
     <div class="login-box">
         <h2>Recuperación de Cuenta</h2>
@@ -127,6 +78,7 @@
     </div>
 </body>
 </html>
+
 <style>
     /*esto es para que la pagina sea responsive en pocas palabras se acomode al tamaño de la ventana coloquenlo donde puedan */
     @media only screen and (max-width: 1200px) {
