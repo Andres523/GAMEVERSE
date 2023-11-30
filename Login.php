f9a32fac4a39593ee1b8841880d8e245ed217c8e
Andres523 <andresposadat523@gmail.com>
2023-11-29 23:28:54 -0500

    listo por hoy

--- Login.php
+++ Login.php
@@ -11,6 +11,7 @@ session_start();
 </head>
 <body>
   
+  
   <!-- Animacion -->
   <svg version="1.1" xmlns="http://www.w3.org/2000/svg"
 		xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="100%" height="100%" viewBox="0 0 1600 900" preserveAspectRatio="xMidYMax slice">
--- javascript.js
+++ javascript.js
@@ -1,15 +1,33 @@
+// Funciones para mostrar y ocultar el modal de confirmación
 function showConfirmationModal() {
     var modal = document.getElementById("confirmation-popup");
-    modal.style.display = "block";
+    if (modal) {
+        modal.style.display = "block";
+    }
 }
 
 function hideConfirmationModal() {
     var modal = document.getElementById("confirmation-popup");
-    modal.style.display = "none";
+    if (modal) {
+        modal.style.display = "none";
+    }
 }
 
-document.getElementById("cancel-button").addEventListener("click", function() {
-    hideConfirmationModal();
+
+document.addEventListener("DOMContentLoaded", function() {
+    
+    var logoutButton = document.querySelector("[name='logout']");
+    if (logoutButton) {
+        logoutButton.addEventListener("click", function(event) {
+            showConfirmationModal();
+        });
+    }
+
+    
+    var cancelButton = document.getElementById("cancel-button");
+    if (cancelButton) {
+        cancelButton.addEventListener("click", function() {
+            hideConfirmationModal();
+        });
+    }
 });
-
-
