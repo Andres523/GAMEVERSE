96f0e8a7233dc9ec93e6db1b435d4333fc0aded4
Andres523 <andresposadat523@gmail.com>
2024-02-08 01:46:22 -0500

    a

--- usuarios.php
+++ usuarios.php
@@ -367,67 +367,10 @@ $conexion->close();
 
 
                                                     
-                <section id="content2">
-                    <h2>Productos</h2>
-                    <button class="btn4" id="openModalBtn">Agregar Producto</button>
-                    <br>
-                    <br>
-
-                                <!-- MODAL PRODUCTOS -->
-                                <div id="productModal" class="modal">
-                                    <div class="modal-content">
-                                        <span class="close" id="closeProductModalBtn">&times;</span>
-                                        <form id="productForm" action="procesar_producto.php" method="post" enctype="multipart/form-data">
-                                            <div class="form-container">
-                                                <div class="form-column">
-                                                    <label for="nombre"></label>
-                                                    <input type="text" id="nombre" name="nombre" placeholder="Nombre" required="Complete este campo">
-
-                                                    <label for="descripcion"></label>
-                                                    <input type="text" id="descripcion" name="descripcion" placeholder="Descripción" required="Complete este campo">
-
-                                                    <label for="requisitos"></label>
-                                                    <input type="text" id="requisitos" name="requisitos" placeholder="Requisitos" required="Complete este campo">
-                                                
-                                                    <label for="cantidad"></label>
-                                                    <input type="number" id="cantidad" name="cantidad" placeholder="Cantidad" required="Complete este campo">
-                                                    
-                                                    <label for="precio"></label>
-                                                    <input type="number" id="precio" name="precio" placeholder="Precio" required="Complete este campo">
-                                                    <br>
-                                                    <label for="imagen">Imagen    </label>
-                                                    <input type="file" id="imagen" name="imagen" accept="image/*" placeholder="Imagen" required="Complete este campo">
-                                                    <br>
-                                                    <label for="video_mp4">Video MP4</label>
-                                                    <input type="file" id="video_mp4" name="video_mp4" accept="video/mp4"placeholder="Video MP4" required="Complete este campo">
-                                                    
-                                                </div>
-                                            </div>
 
 
-                                                <div class="form-column">
-                                                <ul>
-                                                    <h2>Categorías</h2>
-                                                    <?php foreach ($categorias as $categoria): ?>
-                                                        <li>
-                                                        <div class="checkbox">
-  
-                                                            <input class="checkbox__input" type="checkbox" id="categoria_<?php echo $categoria['id']; ?>" name="categorias[]" value="<?php echo $categoria['id']; ?>">
 
-                                                            <label class="checkbox__label" for="categoria_<?php echo $categoria['id']; ?>"><?php echo $categoria['nombre']; ?>
-                                                            <span class="checkbox__custom"></span>
-                                                            </label>
-                                                        </div>    
-                                                        </li>
-                                                    <?php endforeach; ?>
-                                                </ul>
-                                                </div>
-                                                <center>
-                                                <a href="./categorias.php">Agregar Categoría</a>
-                                                <br>
-                                                <button class="btn4" type="submit" name="guardarProducto">Guardar Producto</button>
-                                                </center>
-                                            </form>
+
 
 
 
@@ -456,73 +399,7 @@ $conexion->close();
                                         </div>
                                         </div>
 
-                                <div class="">          
-                                    <div class="tabl">     
-                                    <?php
-                                    $conexion = mysqli_connect("127.0.0.1", "root", "", "gameverse");
 
-                                    if (!$conexion) {
-                                        die("Error de conexión: " . mysqli_connect_error());
-                                    }
-                                
-                                    $consulta = "SELECT id, nombre, descripcion, requisitos, cantidad, precio, imagen FROM productos";
-                                    $resultado = mysqli_query($conexion, $consulta);
-                                
-                                    if (mysqli_num_rows($resultado) > 0) {
-                                        echo '<table>';
-                                        echo '<tr>
-                                                <th>ID: </th>
-                                                <th>Imagen: </th>
-                                                <th>Nombre: </th>
-                                                <th>Descripción: </th>
-                                                <th>Requisitos: </th>
-                                                <th>Cantidad: </th>
-                                                <th>Precio: </th>
-                                                <th>Acciones: </th>
-                                              </tr> ';
-                                    
-                                        while ($fila = mysqli_fetch_assoc($resultado)) {
-                                            echo '<tr>';
-                                            echo '<td>' . $fila['id'] . '</td>';
-                                            echo '<td><img src="' . $fila['imagen'] . '" alt="' . $fila['nombre'] . '" height="50"></td>';
-                                            echo '<td>' . $fila['nombre'] . '</td>';
-                                            echo '<td>' . $fila['descripcion'] . '</td>';
-                                            echo '<td>' . $fila['requisitos'] . '</td>';
-                                            echo '<td>';
-                                            if ($fila['cantidad'] == 0) {
-                                                echo '<span style="color: red;">Agotado</span>';
-                                            } else {
-                                                echo $fila['cantidad'];
-                                            }
-                                            echo '</td>';
-                                            echo '<td style="color: green;">' . $fila['precio'] . ' COP' . '</td>';
-                                            echo '<td> 
-                                            <button class="edit-button" type="button" onclick="editarProducto(' . $fila['id'] . ')">
-                                            <svg class="edit-svgIcon" viewBox="0 0 512 512">
-                                                    <path d="M410.3 231l11.3-11.3-33.9-33.9-62.1-62.1L291.7 89.8l-11.3 11.3-22.6 22.6L58.6 322.9c-10.4 10.4-18 23.3-22.2 37.4L1 480.7c-2.5 8.4-.2 17.5 6.1 23.7s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L387.7 253.7 410.3 231zM160 399.4l-9.1 22.7c-4 3.1-8.5 5.4-13.3 6.9L59.4 452l23-78.1c1.4-4.9 3.8-9.4 6.9-13.3l22.7-9.1v32c0 8.8 7.2 16 16 16h32zM362.7 18.7L348.3 33.2 325.7 55.8 314.3 67.1l33.9 33.9 62.1 62.1 33.9 33.9 11.3-11.3 22.6-22.6 14.5-14.5c25-25 25-65.5 0-90.5L453.3 18.7c-25-25-65.5-25-90.5 0zm-47.4 168l-144 144c-6.2 6.2-16.4 6.2-22.6 0s-6.2-16.4 0-22.6l144-144c6.2-6.2 16.4-6.2 22.6 0s6.2 16.4 0 22.6z"></path>
-                                                    </svg></button>
-                                                    
-                                                    <button class="delete-button" type="button" onclick="eliminarProducto(' . $fila['id'] . ')"><svg class="delete-svgIcon" viewBox="0 0 448 512">
-                                                    <path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"></path>
-                                                    </svg></button>
-                                                  </td>';
-                                            
-                                                  
-                                            
-                                            echo '</tr>';
-                                            echo '<td> <hr> </td>';
-                                        
-                                        }
-                                        
-                                        echo '</table>';
-                                       
-                                    } else {
-                                        echo 'No hay productos disponibles.';
-                                    }
-                                
-                                    mysqli_close($conexion);
-                                    ?>
-                                </div>
 
 
                 </section>
