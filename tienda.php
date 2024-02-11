1ce62e291a5b1e4e982ac286c8f7e9b4e34d39bf
Andres523 <andresposadat523@gmail.com>
2024-02-10 22:56:32 -0500

    a

--- tienda.php
+++ tienda.php
@@ -1,6 +1,25 @@
 <?php
 session_start();
 $loggedIn = isset($_SESSION['nombreUsuario']);
+
+function imprimirEstrellas($calificacion) {
+    if ($calificacion === null) {
+        return 'Sin calificación'; 
+    }
+
+    $estrellas = '';
+    $calificacion = round($calificacion); // Redondear la calificación
+
+    
+    for ($i = 0; $i < 5; $i++) {
+        if ($i < $calificacion) {
+            $estrellas .= '<span style="color: gold;">★</span>'; // Estrella llena
+        } else {
+            $estrellas .= '<span style="color: grey;">★</span>'; // Estrella vacía
+        }
+    }
+    return $estrellas;
+}
 ?>
 
 <!DOCTYPE html>
@@ -111,6 +130,7 @@ $loggedIn = isset($_SESSION['nombreUsuario']);
                             LEFT JOIN calificaciones c ON p.id = c.id_juego
                             GROUP BY p.id";
                         $resultado = mysqli_query($conexion, $consulta);
+                       
 
                         if ($resultado) {
                             while ($fila = mysqli_fetch_assoc($resultado)) {
@@ -119,8 +139,7 @@ $loggedIn = isset($_SESSION['nombreUsuario']);
                                 echo '<img src="' . $fila['imagen'] . '" alt="' . $fila['nombre'] . '" style="width: 100%; min-height: 100%; object-fit: cover;">';
                                 echo '<figcaption>';
                                 echo '<h2 style="text-decoration: none;">' . $fila['nombre'] . '</h2>';
-                                echo '<p>' . round($fila['calificacion_promedio'], 1) . '</p>';
-
+                                echo '<p>' . imprimirEstrellas($fila['calificacion_promedio']) . '</p>';
                                 if ($fila['cantidad'] > 0) {
                                     echo '<p style="color: green;">Precio: $' . $fila['precio'] . '</p>';
                                 } else {
