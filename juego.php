266b9dbe9b8a6ec30c521a49b2ccf77cc6a07907
Sheidel1001 <mari.g.r1001@gmail.com>
2024-02-11 02:34:11 -0500

    ss

--- juego.php
+++ juego.php
@@ -411,16 +411,17 @@ if (isset($_SESSION['nombreUsuario'])) {
         // Si el juego no está en la lista de deseos, muestra el botón para agregarlo a la lista
         echo '<form action="agregar_deseo.php" method="post">';
         echo '<input type="hidden" name="id_juego" value="' . $id_juego . '">';
-        echo '<button type="submit" name="agregar_deseo"><span class="IconContainer">
+        echo '<button class="bookmarkBtn" type="submit" name="agregar_deseo"><span class="IconContainer">
         <svg viewBox="0 0 384 512" height="0.9em" class="icon">
           <path
             d="M0 48V487.7C0 501.1 10.9 512 24.3 512c5 0 9.9-1.5 14-4.4L192 400 345.7 507.6c4.1 2.9 9 4.4 14 4.4c13.4 0 24.3-10.9 24.3-24.3V48c0-26.5-21.5-48-48-48H48C21.5 0 0 21.5 0 48z"
           ></path>
         </svg>
       </span>
-      <p class="text"></p></button>';
+      <p class="text">FAV</p></button>';
         echo '</form>';
     }
+
 
     // Cierra la conexión a la base de datos
     mysqli_close($conexion);
@@ -435,7 +436,9 @@ if (isset($_SESSION['nombreUsuario'])) {
 
                         echo '<form action="carrito.php" method="post">';
                         echo '<input type="hidden" name="id_juego" value="' . $id_juego . '">';
-                        echo '<button type="submit" name="agregar_carrito">Agregar al Carrito</button>';
+                        echo '<button class="CartBtn" type="submit" name="agregar_carrito"><span class="IconCompra"> 
+                        <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512" fill="rgb(17, 17, 17)" class="cart"><path d="M0 24C0 10.7 10.7 0 24 0H69.5c22 0 41.5 12.8 50.6 32h411c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3H170.7l5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24 10.7 24 24s-10.7 24-24 24H199.7c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5H24C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z"></path></svg>
+                      </span><p class="text">Agregar al carrito</p></button>';
                         echo '</form>';
                         echo '<p style="color: green; text-align: center; font-size: 24px;">Precio: $' . $fila['precio'] . " COP".'</p>';
                         ?>
@@ -527,7 +530,6 @@ if (isset($_SESSION['nombreUsuario'])) {
 ?>
 <style>
 .bookmarkBtn {
-  width: 100px;
   height: 40px;
   border-radius: 40px;
   border: 1px solid rgba(255, 255, 255, 0.349);
@@ -584,5 +586,69 @@ if (isset($_SESSION['nombreUsuario'])) {
 .bookmarkBtn:active {
   transform: scale(0.95);
   transition-duration: 0.3s;
+}
+//carrito de compras//
+.CartBtn {
+  width: 140px;
+  height: 40px;
+  border-radius: 12px;
+  border: none;
+  background-color: rgb(255, 208, 0);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  transition-duration: .5s;
+  overflow: hidden;
+  box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.103);
+  position: relative;
+}
+
+.IconCompra {
+  position: absolute;
+  left: -50px;
+  width: 30px;
+  height: 30px;
+  background-color: transparent;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  overflow: hidden;
+  z-index: 2;
+  transition-duration: .5s;
+}
+
+.icon {
+  border-radius: 1px;
+}
+
+.text {
+  height: 100%;
+  width: fit-content;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: rgb(17, 17, 17);
+  z-index: 1;
+  transition-duration: .5s;
+  font-size: 1.04em;
+  font-weight: 600;
+}
+
+.CartBtn:hover .IconContainer {
+  transform: translateX(58px);
+  border-radius: 40px;
+  transition-duration: .5s;
+}
+
+.CartBtn:hover .text {
+  transform: translate(10px,0px);
+  transition-duration: .5s;
+}
+
+.CartBtn:active {
+  transform: scale(0.95);
+  transition-duration: .5s;
 }
 </style>
